7986aa5f3615ea09a6f9d34f2c727d6f67a60588
Augur <augur@BlokhIN.local>
2023-12-25 17:31:43 +0300

    New structure

---
+++ HomeWork/c#/Seminar01/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    System.Console.WriteLine($"Число {number} четное!");
+}
+else 
+{
+    System.Console.WriteLine($"число {number} не четное!");
+}
