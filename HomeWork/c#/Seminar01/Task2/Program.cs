d73b5b5a2c1b32cf7fdfd5cc813ddb96e2b2e52c
Igor Blokhin <iBlokhin@mail.ru>
2024-01-12 17:00:17 +0300

    Внес изменения в Task2

--- HomeWork/c#/Seminar01/Task2/Program.cs
+++ HomeWork/c#/Seminar01/Task2/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Внутри класса Answer напишите метод FindMax, который принимает 
 // на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
 Console.Write("Введите первое число: ");
 int Num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
@@ -15,7 +17,7 @@ if (Num2 > Max)
 if (Num3 > Max)
 {
     Max = Num3;
-    Console.WriteLine(Max);
+    Console.WriteLine($"Максимальное число из введенных => {Max}!");
 }
 else if (Num1 == Num2 && Num2 == Num3)
 {
